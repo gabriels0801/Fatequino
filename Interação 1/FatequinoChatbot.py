264cddc2c35a9811e0e21305e7b4ad3a0b8c45ec
gabriels0801 <88942544+gabriels0801@users.noreply.github.com>
2021-08-14 14:49:39 -0300

    Retirando prints do FatequinoChatBot

--- Interação 1/FatequinoChatbot.py
+++ Interação 1/FatequinoChatbot.py
@@ -18,19 +18,17 @@ class FatequinoChatbot():
         print(mensagemRecebida)
         # verifica se o bot consegue responder a mensagem
         if ( float(self.bot.get_response(mensagemRecebida).confidence) > 0.5):
-            print('show')
             return self.bot.get_response(mensagemRecebida)
         # verifica se o bot sabe responder com as conversas registradas no arquivo conversas.json    
         if mensagemRecebida in self.conversas:
-            print('show')
             return self.bot.get_response(mensagemRecebida)
-        else:
-            # grava as conversas que o bot nao sabe responder em um arquivo
-            if not (mensagemRecebida in self.conversas):
-                self.conversasDesconhecidas.append(mensagemRecebida)
-                with open('conversasSemResposta.json', 'w', encoding='utf-8') as gravarConversa:
-                    json.dump(self.conversasDesconhecidas, gravarConversa, ensure_ascii=False, indent=4, separators=(',', ':'))
-                return "Ainda não sei te responder sobre isso, mas irei pesquisar para conseguir te responder."
+       
+        # grava as conversas que o bot nao sabe responder em um arquivo
+        if not (mensagemRecebida in self.conversas):
+            self.conversasDesconhecidas.append(mensagemRecebida)
+            with open('conversasSemResposta.json', 'w', encoding='utf-8') as gravarConversa:
+                json.dump(self.conversasDesconhecidas, gravarConversa, ensure_ascii=False, indent=4, separators=(',', ':'))
+            return "Ainda não sei te responder sobre isso, mas irei pesquisar para conseguir te responder."
 
     # carrega dados inseridos pelo postman no banco de dados mongoDB
     def setHorarios(self, data):
